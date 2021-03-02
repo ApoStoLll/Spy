31f952a1ab944bc40151b45ed05e8ef1a7f2cfe8
neket <neketapostolov@gmail.com>
2021-03-02 22:07:18 +0200

    binding splash

--- app/src/main/java/com/missclick/spy/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/missclick/spy/ui/splash/SplashFragment.kt
@@ -1,8 +1,10 @@
 package com.missclick.spy.ui.splash
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.missclick.spy.R
+import com.missclick.spy.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash){
-
+    private val binding by viewBinding(FragmentSplashBinding::bind)
 }
