a5940a15accd4969eb4cf6f5a3529fdc60a76f35
neket <neketapostolov@gmail.com>
2021-03-18 00:25:01 +0200

    guide

--- app/src/main/java/com/missclick/spy/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/missclick/spy/adapters/ViewPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.missclick.spy.ui.viewpager.GuideFragment
 
 class ViewPagerAdapter(fragment : Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 5
+    override fun getItemCount(): Int = 7
 
     override fun createFragment(position: Int): Fragment {
         val fragment = GuideFragment()
--- app/src/main/java/com/missclick/spy/ui/viewpager/GuideFragment.kt
+++ app/src/main/java/com/missclick/spy/ui/viewpager/GuideFragment.kt
@@ -1,6 +1,7 @@
 package com.missclick.spy.ui.viewpager
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -23,7 +24,39 @@ class GuideFragment : Fragment(R.layout.fragment_guide){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tv.text = param1.toString()
+        //binding.tv.text = param1.toString()
+        when(param1){
+            0 -> binding.apply {
+                titleText.text = getString(R.string.first_page_title)
+                descriptionText.text = getString(R.string.first_page_text)
+            }
+            1 -> binding.apply {
+                titleText.text = getString(R.string.second_page_title)
+                descriptionText.text = getString(R.string.second_page_text)
+            }
+            2 -> binding.apply {
+                titleText.text = getString(R.string.third_page_title)
+                descriptionText.text = getString(R.string.third_page_text)
+            }
+            3 -> binding.apply {
+                titleText.text = getString(R.string.fourth_page_title)
+                descriptionText.text = getString(R.string.fourth_page_text)
+            }
+            4 -> binding.apply {
+                titleText.text = getString(R.string.fifth_page_title)
+                descriptionText.text = getString(R.string.fifth_page_text)
+            }
+            5 -> binding.apply {
+                titleText.text = getString(R.string.sixth_page_title)
+                descriptionText.text = getString(R.string.sixth_page_text)
+                //descriptionText.textSize = resources.getDimension(R.dimen.text_small)
+                descriptionText.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_small))
+            }
+            6 -> binding.apply {
+                titleText.text = getString(R.string.seventh_page_title)
+                descriptionText.text = getString(R.string.seventh_page_text)
+            }
+        }
     }
 
     companion object{
